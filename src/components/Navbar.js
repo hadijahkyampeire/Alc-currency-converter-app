ae4140a95755c5444ff96ca3c28072b7ba61e375
Kyampeire Hadijah <Hadijahk@Erics-MacBook-Pro.local>
2018-06-29 20:12:08 +0300

    Change color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Navbar=()=>(
 <div>
-<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+<nav class="navbar navbar-expand-lg navbar-light nav">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="/">Currency Converter App <span class="sr-only">(current)</span></a>
       </li>
     </ul>
   </div>
